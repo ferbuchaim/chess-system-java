207a68f051fa0745a0cad22e3a61df3ee1bf5c29
ferbuchaim <buchaim.fernando@gmail.com>
2022-05-20 16:00:44 -0300

    Class Position created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Starting first project!");
+		Position p1 = new Position(3,5);
+		Position p2 = new Position(2,4);
 		
+		System.out.println(p1);
+		System.out.println(p2);
 	}
 
 }
