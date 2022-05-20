bcd54035f1d286e20175bc9edeff6e4567db4a63
ferbuchaim <buchaim.fernando@gmail.com>
2022-05-20 16:16:51 -0300

    Classes Board and Peace created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,12 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position p1 = new Position(3,5);
-		Position p2 = new Position(2,4);
-		
-		System.out.println(p1);
-		System.out.println(p2);
+		Board bord1 = new Board(8,8);
 	}
 
 }
